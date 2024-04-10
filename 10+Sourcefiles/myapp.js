92582d0ee4e449f7cc7526c57a1d9a8402183ccf
Osasere Ikponmwosa <edenosas1@gmail.com>
2024-04-10 16:18:13 +0100

    functions and subroutines

--- 10+Sourcefiles/myapp.js
+++ 10+Sourcefiles/myapp.js
@@ -1,13 +1,26 @@
-console.log( "Hello world" );
+// console.log( "Hello world" );
 
-console.log( 200 );
+// console.log( 200 );
 
-console.log( true );
+// console.log( true );
 
-console.log( null );
+// console.log( null );
 
-var num = 10;
+// var num = 10;
 
-console.log( num );
+// console.log( num );
 
-console.log( "string", 10.11, true, false, null, num );
+// console.log( "string", 10.11, true, false, null, num );
+
+function makeCoffee( sugar, milk) {
+    var instructions = "Boil water,"
+    
+    instructions += " pour into cup, ";
+    instructions +=  " add coffee granules, ";
+    instructions +=  " add " + sugar + " spoons of sugar, ";	
+    instructions +=  " add " + milk + "% milk. ";	
+
+    return instructions;
+}
+
+console.log( makeCoffee( 2, 20 ) );
