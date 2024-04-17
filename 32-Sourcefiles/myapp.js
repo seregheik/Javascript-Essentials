416aa73420bb42106bb68e8d6a809a8b508b8540
Osasere Ikponmwosa <edenosas1@gmail.com>
2024-04-17 15:49:40 +0100

    Targeting DOM elements

---
+++ 32-Sourcefiles/myapp.js
@@ -0,0 +1,24 @@
+// Any method with element will return a single object.
+// Any method with elements will return an array.
+
+console.log( document.getElementsByTagName('p') );
+
+console.log( document.getElementById('hello') );
+
+console.log( document.getElementsByClassName('pClass') );
+
+console.log( 
+    document.querySelectorAll( 
+        ' p[data-content="123"], body > h1.pClass > span '
+    ) 
+);
+
+
+
+function classtextchanger(classname, newtext){
+    document.getElementById(classname).innerText = newtext;
+}
+
+classtextchanger('hello', 'hi')
+
+console.log(classtextchanger);
