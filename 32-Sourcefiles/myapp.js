5c3064279faf2cddedf42825890344e8f4004eb0
Osasere Ikponmwosa <edenosas1@gmail.com>
2024-04-20 16:06:52 +0100

    javascript styling

--- 32-Sourcefiles/myapp.js
+++ 32-Sourcefiles/myapp.js
@@ -1,24 +1,31 @@
 // Any method with element will return a single object.
 // Any method with elements will return an array.
 
-console.log( document.getElementsByTagName('p') );
+// console.log( document.getElementsByTagName('p') );
 
-console.log( document.getElementById('hello') );
+// console.log( document.getElementById('hello') );
 
-console.log( document.getElementsByClassName('pClass') );
+// console.log( document.getElementsByClassName('pClass') );
 
-console.log( 
-    document.querySelectorAll( 
-        ' p[data-content="123"], body > h1.pClass > span '
-    ) 
-);
+// console.log( 
+//     document.querySelectorAll( 
+//         ' p[data-content="123"], body > h1.pClass > span '
+//     ) 
+// );
 
 
 
-function classtextchanger(classname, newtext){
-    document.getElementById(classname).innerText = newtext;
-}
+// function classtextchanger(classname, newtext){
+//     document.getElementById(classname).innerText = newtext;
+// }
 
-classtextchanger('hello', 'hi')
+// classtextchanger('hello', 'hi')
 
-console.log(classtextchanger);
+// console.log(classtextchanger);
+
+var styleme = document.getElementById('styleme');
+
+styleme.style.background = 'red';
+styleme.style.color = 'white';
+
+console.log(getComputedStyle(styleme))
