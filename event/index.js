d56ac033d8b9b6a47b30dc0e585166bd61677e78
Osasere Ikponmwosa <edenosas1@gmail.com>
2024-04-21 10:21:45 +0100

    working on project

--- event/index.js
+++ event/index.js
@@ -1,11 +1,13 @@
-let a = Number(document.getElementsByClassName('clicked')[0].innerHTML);
-additional =(aa)=>{
-    aa += 1;
-    return aa
-}
-clicker = () => {
-    a = additional(a)
-    console.log(a);
-    document.getElementsByClassName('clicked')[0].innerHTML = a;
-}
+var number = document.createElement('div');
+
+var clickerer = document.getElementsByClassName('cars')[0]
+
+var clickererr = document.getElementsByClassName('cars') 
+console.log(clickererr)
+clickerer.addEventListener('click', function () {
+    let htmlNum = document.getElementsByClassName('clicked')[0];
+    counter = Number(htmlNum.innerHTML)
+    counter += 1
+    htmlNum.innerHTML = counter
+});
 
---
+++ myapp.js
@@ -0,0 +1,7 @@
+let colorwidget = document.getElementById('background');
+let widthwidget = document.getElementById('width');
+let heightwidget = document.getElementById('height');
+
+function set () {
+    
+};
