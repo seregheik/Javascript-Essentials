9f597617919bbab2c3a4ae716fcde5db60dbd0d0
Osasere Ikponmwosa <edenosas1@gmail.com>
2024-04-21 20:45:53 +0100

    still working on simple task o

--- myapp.js
+++ myapp.js
@@ -1,7 +1,18 @@
 let colorwidget = document.getElementById('background');
 let widthwidget = document.getElementById('width');
 let heightwidget = document.getElementById('height');
+let optionsWidget = document.getElem
+let setButton = document.getElementById('set');
 
+
+setButton.addEventListener('click', set);
 function set () {
-    
+    let classes = []
+    function looping(option){
+        for (i=0; i<option.length; i++) {
+            console.log(option[i].getAttribute('value'));
+        }
+    };
+   looping(optionsWidget)
+   console.log(classes);
 };
