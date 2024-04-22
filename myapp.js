13c069e3ee7b57800621236d4f3fedb2964d48e4
Osasere Ikponmwosa <edenosas1@gmail.com>
2024-04-22 10:21:49 +0100

    finally done

--- myapp.js
+++ myapp.js
@@ -1,18 +1,22 @@
-let colorwidget = document.getElementById('background');
-let widthwidget = document.getElementById('width');
-let heightwidget = document.getElementById('height');
-let optionsWidget = document.getElementsByName('select')
+let optionsWidget = document.getElementsByName('cssProperties')
 let setButton = document.getElementById('set');
+let box = document.getElementById('box');
 
 
 setButton.addEventListener('click', set);
+
+// console.log(optionsWidget)
 function set () {
-    let classes = []
     function looping(option){
+        userValue = []
         for (i=0; i<option.length; i++) {
-            console.log(option[i].getAttribute('value'));
+            console.log(option[i].value);
+            userValue.push(option[i].value)
         }
+        console.log(userValue)
+        box.className = userValue[0];
+        box.classList.add(userValue[1]);
+        box.classList.add(userValue[2]);
     };
    looping(optionsWidget)
-   console.log(classes);
 };
